825ba91b891a1396d10567bbeb8aa1c80e922a5a
Daniel Melo <dcm189497@gmail.com>
2022-06-26 19:17:15 +0100

    up condicao js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,17 @@
-function hora() {
-    let hora = document.getElementById('hora1');
-    let horaAlterada = Number(hora.value);
-    let result = document.getElementById('title');
-    let filtro = document.getElementById('container-filter');
-    let botaoRecarregar = document.getElementById('botao');
-
-
-    if (horaAlterada >= 6 && horaAlterada <= 12) {
-        result.innerHTML = 'Bom dia';
-        result.style.color = 'white'
-        filtro.style.backgroundColor = 'rgba(238, 255, 0, 0.20)'
-    } else if (horaAlterada > 12 && horaAlterada < 18) {
-        result.innerHTML = 'Boa tarde';
-        filtro.style.backgroundColor = 'rgba(56, 217, 223, 0.377)'
-    } else if (horaAlterada >= 0 && horaAlterada < 6) {
-        result.innerHTML = 'É madruga, vai dormir';
-        result.style.color = 'white'
-        filtro.style.backgroundColor = 'rgba(2, 5, 37, 0.596)'
-    } else if (horaAlterada > 23) {
-        result.innerHTML = '!!!! Este horário não exite !!!!';
-        filtro.style.backgroundColor = 'red'
+function carregar() {
+    let msg = document.getElementById('msg');
+    let img = document.getElementById('imagem');
+    let data = new Date();
+    let hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas`;
+    if (hora >= 0 && hora < 12) {
+        img.src = 'assets/images/manha.jpg';
+       document.body.style.backgroundColor = '#ffd861'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'assets/images/tarde.jpg';
+        document.body.style.backgroundColor = '#837023'
     } else {
-        result.innerHTML = 'Boa noite';
-        filtro.style.backgroundColor = 'rgba(4, 14, 100, 0.596)'
+        img.src = 'assets/images/noite.jpg'
+        document.body.style.backgroundColor = '#110e21'
     }
 }
-
